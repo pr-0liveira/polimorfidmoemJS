e938ba584696d4d11289ea7ab621c5bcf0926cb5
VitorCoffler <vitorcoffler145@gmail.com>
2025-11-12 11:22:00 -0300

    criei a class Conta

--- conta.js
+++ conta.js
@@ -1,16 +1,65 @@
-export class conta {
-    #qtdContas = 0;
+import { Cliente } from "./cliente";
+export class Conta {
+    static #qtdContas = 0;
     #id;
     #saldo;
     #titular;
 
-    constructor(qtdContas, id, saldo, titular) {
-        this.#qtdContas = qtdContas;
-        this.#id = id;
-        this.#saldo = saldo;
-        this.#titular = titular;
+    constructor(cliente, saldo = 0.0) {
+        this.titular = cliente;
+        this.#saldo = saldo < 0 ? 0.0 : saldo;
+        Conta.#qtdContas++;
+        this.#id = "" + new Date().getFullYear() + Conta.#qtdContas;
+    }
+    get id() {
+        return this.#id;
+    }
+    static get qtdContas() {
+        return Conta.#qtdContas;
+    }
+    get saldo() {
+        return this.#saldo;
+    }
+    get titular() {
+        return this.#titular;
+    }
+    set titular(cliente) {
+        if (cliente != undefined && cliente instanceof Cliente) {
+            this.#titular = cliente;
+            return cliente;
+        } else {
+            return null;
+        }
     }
 }
 
+sacar(valor){
+    if (valor > 0 && valor <= this.#saldo) {
+        this.#saldo -= valor;
+        return true;
+    }
+    return false;
+}
+
+depositar(valor){
+    if (valor > 0.00) {
+        this.#saldo += valor;
+        return true;
+    }
+    return false;
+}
+
+transferir(valor, contaDestino){ 
+    if (contaDestino instanceof Conta && this.sacar(valor)) {
+        contaDestino.depositar(valor);
+        return true;
+    }
+    return false;
+}
 
 
+toString(){
+    return ("Nº Conta= "+ this.#id+
+        "/nTitular= " + this.#titular.toString()+
+        "/nSaldo= " + this.#saldo.toFixed(2));
+    }
